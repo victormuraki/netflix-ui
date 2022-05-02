e52be6bcc0c4a00850a759675ef10a8c81890cd2
Victor Muraki <vmuraki@MKMONDIAL.local>
2022-05-02 14:40:30 -0300

    primeira parte

---
+++ src/components/FeaturedMovie.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './FeaturedMovie.css'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ item }) => {
+
+    let firstDate = new Date(item.first_air_date)
+
+    return (
+        <div>
+            <section className="featured" style={{
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+            }}>
+                <div className="featured--vertical">
+                    <div className="featured-horizontal">
+                        <div className="featured--name">{item.original_name}</div>
+                        <div className="featured--info">
+                            <div className="featured--points">{item.vote_average} pontos</div>
+                            <div className="featured--year"> {firstDate.getFullYear()}</div>
+                            <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                            <div className="featured--description">{item.overview}{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                            <div className="featured--buttons">
+
+                            </div>
+                            <div className="featured-genres"><strong>Gêneros:</strong>... </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
